f38d3785658d006e19bee8ee6695e835d3c2a516
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-02-04 10:33:27 -0800

    removed unecessary parts

--- SeedBank_SingleCell/tools.py
+++ SeedBank_SingleCell/tools.py
@@ -1,9 +1,6 @@
 import plotly.plotly as py
 import plotly.graph_objs as go 
-import plotly.tools as tls
 import numpy as np
-import time
-from subprocess import call
 
 def num_digits(n):
     c = 1
@@ -14,19 +11,6 @@ def num_digits(n):
     
 def generate_image_id(n): #Used to identify images created with an ID
     return "{}{}".format("0"*(3-num_digits(n)),n)
-    
-colorScale = [
-    [0.0, "rgb(165,0,38)"],
-    [0.1111111111111111, "rgb(215,48,39)"],
-    [0.3333333333333333, "rgb(253,174,97)"],
-    [0.4444444444444444, "rgb(254,224,144)"],
-    [0.2222222222222222, "rgb(244,109,67)"],
-    [0.5555555555555556, "rgb(224,243,248)"],
-    [0.6666666666666666, "rgb(171,217,233)"],
-    [0.7777777777777778, "rgb(116,173,209)"],
-    [0.8888888888888888, "rgb(69,117,180)"],
-    [1.0, "rgb(49,54,149)"]
-]
 
 #Saves existing data as PNG 
 def save_image(name, x, y, xmax, ymax):
