81ba4be2cd91e383911dbf87f8678d9c1bbfafe9
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-02-13 19:05:43 -0800

    trying transition matrices again

--- SeedBank_Aged_SingleCell/README.md
+++ SeedBank_Aged_SingleCell/README.md
@@ -1,6 +1,7 @@
-#SeedBank Single Cell Model
+#Aged SeedBank Single Cell Model
 
 This is a seedbank bank single cell model, where the population produces seeds that may be stored in the seedbank, or may germinate into a plant
+
 Seeds from the seedbank may germinate at a later date, but do not survive past 2 steps
 
 Environmental factors are not included in this model for the likelihood of germination and living rates
@@ -13,4 +14,4 @@ tools.py contains functions for graphing, and other functions not related to the
 ##Graphs
 population_plot.gif is a graph of the density of the population in the cell over time
 
-seedbank_plot.gif is a graph of the seeds in the cell over time
+seedbank_plot.gif is a graph of the seeds in both aged seedbanks in the cell over time
--- SeedBank_Aged_SingleCell/main.py
+++ SeedBank_Aged_SingleCell/main.py
@@ -1,1 +1,37 @@
 from tools import *
+
+#Constants
+T = 50
+e = .7
+g = .9
+l = .6
+M = np.arange(float(4)).reshape((float(2),float(2)))
+M[:,:] = 0.0
+M[0,0] = 0
+M[0,1] = e
+M[1,0] = g
+M[1,1] = 0
+
+def main():
+    #Data
+    # X[0] = Seeds
+    # X[1] = Flowering Plants 
+    X = np.arange(float(T*2)).reshape((float(2), float(T)))
+    X[:,:] = 0.0
+    X[1,0] = .1
+    X[0,0] = e*.2
+
+    for t in range(0, T-1):
+        X_t = np.arange(float(2*1)).reshape((float(2),float(1)))
+        X_t[:,:] = 0.0
+        X_t[0,0] = X[0,t]
+        X_t[1,0] = X[1,t]
+        X_tp1 = np.matmul(M, X_t)
+        X[0,t+1] = X_tp1[0,0]
+        X[1,t+1] = X_tp1[1,0]
+    print X
+    for t in range(0, T-1):
+        print "Begin T: {}".format(t)
+        save_image("population",range(0, t+2), X[1,:t+2], T, max(X[1]))
+
+main()
