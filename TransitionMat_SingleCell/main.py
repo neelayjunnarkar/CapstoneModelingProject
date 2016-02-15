9cba2fc8fa60cc14bad5d31dcd1cc0b8b7fceef8
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-02-15 14:42:21 -0800

    reworked transition matrix to use matplotlib

--- TransitionMat_SingleCell/main.py
+++ TransitionMat_SingleCell/main.py
@@ -1,21 +1,59 @@
-from tools import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+print "Dependencies loaded..."
 
-rm = 0.2 # Maximum growth rate
-K  = 1.0 # Carrying Capacity
-M = np.arange(float(1)).reshape((float(1), float(1)))
-M[:] = 0.0 
-T = 30
+T = 100
+
+gm = 0.94
+l = 0.9
+
+M = np.arange(float(2.0*2.0)).reshape((float(2.0),float(2.0)))
+X = np.arange(float(2.0*T)).reshape(2.0,float(T))
+M[:,:] = 0.0
+M[0,0] = 0
+M[0,1] = 1-l
+M[1,0] = gm
+M[1,1] = l
+
+X[:,:] = 0.0
+X[0,0] = 0
+X[1,0] = 2
+fig = plt.figure()
+axess = fig.add_subplot(211, xlim=(0.0,T), ylim=(0.0, 40))
+axesp = fig.add_subplot(212, xlim=(0.0,T), ylim=(0.0,40))
+
+seedbank, = axess.plot(range(T), X[0])
+plantpop, = axesp.plot(range(T), X[1])
+
+def update_data(t):
+    print "Updating data..."
+    X_t = np.arange(2.0*1.0).reshape((float(2.0),float(1.0)))
+    X_t[:,:] = 0.0
+    X_t[0,0] = X[0,t]
+    X_t[1,0] = X[1,t]
+    X_tp1 = np.matmul(M, X_t)
+    X[0,t+1] = X_tp1[0,0]
+    X[1,t+1] = X_tp1[1,0]
+    if t == T-2:
+        print "Data: {}".format(X[:t+2, :t+2])
+
+def update_graph(t):
+    print "Updating graph..."
+    update_data(t)
+    seedbank.set_data(range(t+1), X[0,:t+1])
+    plantpop.set_data(range(t+1), X[1,:t+1])
+    return seedbank,plantpop
+
+def init_graph():
+    seedbank.set_data([], [])
+    plantpop.set_data([],[])
+    return seedbank, plantpop
 
 def main():
-    P = np.arange(float(1*T)).reshape((float(1),float(T)))
-    P[:,:] = 0.0
-    P[0,0] = 0.2
-
-    for t in range(0,T-1):
-        M[0,0] = 1.0+rm*(1-(P[0,t]/K))
-        P[0,t+1] = np.matmul(M, [P[0,t]])
-    print P
-    for t in range(0, T-1):
-        print "Saving Time-Step {}".format(t)
-        save_image("population", range(0, t+2), P[0, :t+2], T, 1.1)
+    print "Beginning animation..."
+    a = anim.FuncAnimation(fig, update_graph, frames=range(T-1), repeat=False, blit=True, interval=10) 
+    a.save("seedbank_singlecell_transmat.mp4", fps=30, extra_args=['-vcodec', 'libx264'])
+    plt.show()
+    print "Showing animation..."
 main()
