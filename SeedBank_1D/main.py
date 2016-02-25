0f176e81734549f12b3023cfa931276f54548678
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-02-25 10:55:55 -0800

    filled in animation/plotting structure and set up constants

--- SeedBank_1D/main.py
+++ SeedBank_1D/main.py
@@ -15,8 +15,26 @@ print "Dependencies loaded..."
 STEP_OUTPUT = True
 print "Output on each step is {}".format("enabled" if STEP_OUTPUT else "disabled")
 
+# Transition Matrix--calculates size of population at next data step
+#      S       P
+#    ---       ---
+# S  | ss(1-g) e |
+#    |           |
+# P  | g       l |
+#    ---       ---
+# S: Seed Bank
+# P: Plant Population
+#
+# ss: Seed Survivorship
+# g:  Germination fraction of seeds
+# e:  Seeds per plant
+# l:  Fraction of plants that live to next generation
+
 # Number of steps the model will be run for
-T = 100
+T = 4
+
+# Number of cells
+N = 3
 
 # Seed Survivorship. Fraction of seeds that remain in seed bank that can germinate
 ss = 0.7
@@ -34,26 +52,33 @@ e = 27
 M = np.arange(float(2.0*2.0)).reshape((float(2.0),float(2.0)))
 
 # Matrix holding seedbank and plant population data
-# Row 0 is seedbank data
-# Row 1 is plant population data
-X = np.arange(float(2.0*T)).reshape(2.0,float(T))
+# [time, cell data, cell]
+# time:      indices[0,T-1]
+# cell data: indices[0,1]
+#   0: Seed Bank
+#   1: Plant Population
+# cell:      indices[0,N-1]
+X = np.arange(float(T*N*2.0)).reshape((float(T),2.0,float(N)))
 
 # Transition Matrix values
 M[:,:] = 0.0
-M[0] = []
-M[1] = []
-print M
+M[0] = [ss*(1-g), e]
+M[1] = [g,        l]
+# print M
 
 # Initial population values
-
+X[:,:,:] = 0
+X[0,0,1] = 23  # Set seedbank of 2nd cell at time 0
+X[0,1,1] = 0.5 # Set population size of 2nd cell at time 0
+  
 # Set up plotting tools
 pop_maxy = 11
 seed_maxy = 300
 fig = plt.figure()
-axesp = fig.add_subplot(211, xlim=(0.0,T), ylim=(0.0, pop_maxy), title='Plant Population')
-axess = fig.add_subplot(212, xlim=(0.0,T), ylim=(0.0, seed_maxy), title='Seed Bank Size' )
-seedbank, = axess.plot(range(T), X[0])
-plantpop, = axesp.plot(range(T), X[1])
+axesp = fig.add_subplot(211, xlim=(0.0,N), ylim=(0.0, pop_maxy), title='Plant Population')
+axess = fig.add_subplot(212, xlim=(0.0,N), ylim=(0.0, seed_maxy), title='Seed Bank Size' )
+seedbank, = axess.plot(range(N), X[0,0])
+plantpop, = axesp.plot(range(N), X[0,1])
 
 def update_data(t):
     """
@@ -71,10 +96,18 @@ def update_graph(t):
     """
     if STEP_OUTPUT:
         print "Updating graph..."
+    update_data(t)
+    seedbank.set_data(range(t+1), )
+    plantpop.set_data(range(t+1), )
+    return seedbank, plantpop
+    
 def init_graph():
     """
     Initializes the plot to nothing
     """
+    plantpop.set_data([],[])
+    seedbank.set_data([],[])
+    return seedbank, plantpop
 
 def main():
     """
